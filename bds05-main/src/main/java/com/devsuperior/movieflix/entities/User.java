89eaa30ecf28e442be6c7b7d1e06271fb8581f47
Murilo Ramos da Silva <murilo.rsilva@totvs.com.br>
2022-08-10 09:08:30 -0300

    Implementação repository e user e roles

--- bds05-main/src/main/java/com/devsuperior/movieflix/entities/User.java
+++ bds05-main/src/main/java/com/devsuperior/movieflix/entities/User.java
@@ -1,17 +1,31 @@
 package com.devsuperior.movieflix.entities;
 
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
 
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable{
+public class User implements UserDetails, Serializable{
 
 
 	private static final long serialVersionUID = 3159586727930825152L;
@@ -22,6 +36,12 @@ public class User implements Serializable{
 	private String name;
 	private String email;
 	private String password;
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_user_role",
+		joinColumns = @JoinColumn(name = "user_id"),
+		inverseJoinColumns = @JoinColumn(name = "role_id"))	
+	private Set<Role> roles = new HashSet<>();
 	
 	public User() {
 	}
@@ -65,12 +85,64 @@ public class User implements Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+	
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		
+		return roles.stream().map(role -> new SimpleGrantedAuthority(role.getAuthority()))
+				.collect(Collectors.toList());
+	}
 
+
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+
+
+	@Override
+	public boolean isAccountNonExpired() {
+
+		return true;
+	}
+
+
+
+	@Override
+	public boolean isAccountNonLocked() {
+		
+		return true;
+	}
+
+
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		
+		return true;
+	}
+
+
+
+	@Override
+	public boolean isEnabled() {
+		
+		return true;
+	}
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
